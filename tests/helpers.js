02fdd8a70e7c50f05615acbd0833f97584f6ea67
Patrick St-Louis <43082425+PatStLouis@users.noreply.github.com>
2024-11-19 23:47:00 -0500

    Revert "add missing helper functions"
    This reverts commit a7e6255e9f77938720d07bda24cc5e2bad1a9bda.

--- tests/helpers.js
+++ tests/helpers.js
@@ -7,7 +7,6 @@ import * as bs64 from 'base64url-universal';
 import {createRequire} from 'node:module';
 import {isUtf8} from 'node:buffer';
 import {klona} from 'klona';
-import {readFileSync} from 'fs';
 import {v4 as uuidv4} from 'uuid';
 
 export const require = createRequire(import.meta.url);
@@ -36,9 +35,6 @@ export const ISOTimeStamp = ({date = new Date()} = {}) => {
  *
  * @returns {Promise<object>} The resulting issuance result.
  */
-
-export const config = JSON.parse(readFileSync('./config/runner.json'));
-
 export const createInitialVc = async ({
   issuer,
   vc,
@@ -273,12 +269,4 @@ export function createValidCredential(version = 2) {
     return null;
   }
   return credential;
-}
-
-export function setupRow() {
-  // append test meta data to the it/test this.
-  this.currentTest.cell = {
-    columnId: this.currentTest.parent.title,
-    rowId: this.currentTest.title
-  };
 }
